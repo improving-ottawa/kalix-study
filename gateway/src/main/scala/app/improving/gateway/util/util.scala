691038523f63b4fa761ea898b23e7cfa52d2971e
Watt Poosanguansit <wpoosanguansit@yahoo.com>
2023-02-05 21:38:57 +0700

    Commit WIP for test prep handleStartScenario.

--- gateway/src/main/scala/app/improving/gateway/util/util.scala
+++ gateway/src/main/scala/app/improving/gateway/util/util.scala
@@ -3,8 +3,8 @@ package app.improving.gateway.util
 import scala.util.Random
 
 object util {
-  def genEmailAddressForTenantName(r: Random, tenantName: String): String =
-    s"${r.nextString(10)}@$tenantName.com}"
+  def genEmailAddressForName(r: Random, name: String): String =
+    s"${r.nextString(10)}@$name.com}"
 
   def genMobileNumber(r: Random): String =
     s"(${r.nextInt()}${r.nextInt()}${r.nextInt()})-${r.nextInt()}${r.nextInt()}${r
